530e98fe989997f994667df8733f07b066da0aa0
VicDario <26585356+VicDario@users.noreply.github.com>
2021-08-27 21:05:30 -0400

    Callbacks and fix error

--- src/callback/main.js
+++ src/callback/main.js
@@ -1,6 +1,7 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let api = 'https://rickandmortyapi.com/api/character/'
 
-function getData(url, callback) {
+function fetchData(url, callback) {
     let xhttp = new XMLHttpRequest();
     xhttp.open('GET', url, true);
     xhttp.onreadystatechange = function() {
@@ -9,6 +10,20 @@ function getData(url, callback) {
         } else {
             const error = new Error('Error ' + url);
             return callback(error, null);
+        }
     }
     xhttp.send();
 }
+
+fetchData(api, function(error, data) {
+    if (error) return console.error(error);
+    fetchData(api + data.results[0].id, function(error2, data2) { 
+        if (error2) return console.error(error2);
+        fetchData(data2.origin.url, function(error3, data3) {
+            if (error3) return console.error(error3);
+            console.log(`Number of characters: ${data.info.count}`);
+            console.log(`Name: ${data2.name}`);
+            console.log(`Origin: ${data3.dimension}`);
+        });
+    });
+});
