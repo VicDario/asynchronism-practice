8151ee760ae53962795adbb7d97c618dd7322279
VicDario <26585356+VicDario@users.noreply.github.com>
2021-08-27 21:52:31 -0400

    Other promise

--- src/promises/main.js
+++ src/promises/main.js
@@ -1,13 +1,30 @@
 const somethingWillHappen = () => {
     return new Promise((resolve, reject) => {
         if (true) {
-            resolve('Hey!');
+            resolve("Hey!");
         } else {
-            reject('Whoooops!');
+            reject(new Error("Whoooops!"));
         }
     });
-}
+};
 
 somethingWillHappen()
-    .then(response => console.log(response))
-    .catch(error => console.error(error));
+    .then((response) => console.log(response))
+    .catch((error) => console.error(error));
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (false) {
+            setTimeout(() => {
+                resolve("Hey!");
+            }, 3000);
+        } else {
+            const error = new Error("Whoooops!");
+            reject(error);
+        }
+    });
+};
+
+somethingWillHappen2()
+    .then((response) => console.log(response))
+    .catch((error) => console.error(error));
