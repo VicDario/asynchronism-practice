225c5e3d42a8b4ebc3b236b2f49dc8b47fe0526d
VicDario <26585356+VicDario@users.noreply.github.com>
2021-08-28 20:52:43 -0400

    ECMA in fetchData

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,17 +1,16 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-function fetchData(url) {
+const fetchData = (url) => {
     return new Promise((resolve, reject) => {
         const xhttp = new XMLHttpRequest();
         xhttp.open('GET', url, true);
-        xhttp.onreadystatechange = function() {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                callback(null, JSON.parse(xhttp.responseText));
-            } else {
-                const error = new Error('Error ' + url);
-                return callback(error, null);
-            }
+        xhttp.onreadystatechange = () => {
+            (xhttp.readyState == 4 && xhttp.status == 200)
+                ?   resolve(JSON.parse(xhttp.responseText))
+                :   reject(new Error('Error ' + url));
         }
         xhttp.send();
     });
 }
+
+module.exports = fetchData;
